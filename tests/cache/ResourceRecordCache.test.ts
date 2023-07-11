fee6ca4cfa77d57d66e0b8481430a27f1cc333a3
Amy <50583248+amydevs@users.noreply.github.com>
2023-07-11 17:23:21 +1000

    chore: ResourceRecordCache overflow tests

--- tests/cache/ResourceRecordCache.test.ts
+++ tests/cache/ResourceRecordCache.test.ts
@@ -2,12 +2,16 @@ import type { CachableResourceRecord, QuestionRecord } from '@/dns';
 import type { MDNSCacheExpiredEvent } from '@/cache/events';
 import { QClass, QType, RClass, RType } from '@/dns';
 import { ResourceRecordCache } from '@/cache';
+import { fc, testProp } from '@fast-check/jest';
+import { resourceRecordArb } from '../dns/utils';
+
+const MAX_RECORDS = 100;
 
 describe(ResourceRecordCache.name, () => {
   let cache: ResourceRecordCache;
 
   beforeEach(async () => {
-    cache = await ResourceRecordCache.createMDNSCache();
+    cache = await ResourceRecordCache.createMDNSCache({ max: MAX_RECORDS });
   });
 
   afterEach(async () => {
@@ -81,6 +85,15 @@ describe(ResourceRecordCache.name, () => {
       unicast: false,
     };
     expect(cache.get(question)).toEqual(records);
+  });
+
+  testProp(
+    'overflow',
+    [fc.array(resourceRecordArb, { minLength: MAX_RECORDS + 1, maxLength: MAX_RECORDS + 1 })],
+    (records) => {
+    cache.set(records as CachableResourceRecord[]);
+    expect(cache.count).toEqual(MAX_RECORDS);
+    expect(cache.get(records[0] as CachableResourceRecord).length).toEqual(0);
   });
 
   test('expiry', async () => {
--- tests/dns/utils.test.ts
+++ tests/dns/utils.test.ts
@@ -22,113 +22,7 @@ import {
   RCode,
   RType,
 } from '@/dns';
-
-const FC_UINT32 = fc.integer({ min: 0, max: 4294967295 }); // 32-Bit Unsigned Integer Limits
-const FC_UINT16 = fc.integer({ min: 0, max: 65535 }); // 16-Bit Unsigned Integer Limits
-
-const FC_PACKET_OPCODES = fc.constantFrom(PacketOpCode.QUERY);
-const FC_RCODES = fc.constantFrom(RCode.NoError);
-
-// For all integers in set of values in QType/QClass
-const FC_QTYPES = fc.constantFrom(
-  QType.A,
-  QType.AAAA,
-  QType.ANY,
-  QType.CNAME,
-  QType.NSEC,
-  QType.OPT,
-  QType.PTR,
-  QType.TXT,
-  QType.SRV,
-);
-const FC_QCLASSES = fc.constantFrom(QClass.ANY, QClass.IN);
-
-const FC_QUESTION = fc.record({
-  name: fc.domain(),
-  type: FC_QTYPES,
-  class: FC_QCLASSES,
-  unicast: fc.boolean(),
-});
-
-const FC_AAAA_RECORD = fc.record({
-  name: fc.domain(),
-  type: fc.constant(RType.AAAA),
-  flush: fc.boolean(),
-  class: fc.constant(RClass.IN),
-  ttl: FC_UINT32,
-  data: fc
-    .ipV6()
-    .filter((ip) => ip.indexOf('.') === -1)
-    .chain((ip) => fc.constant(parseIPv6(generateIPv6(ip)).data)),
-  // Filter out mapped ipv6 addresses
-});
-
-const FC_A_RECORD = fc.record({
-  name: fc.domain(),
-  type: fc.constant(RType.A),
-  flush: fc.boolean(),
-  class: fc.constant(RClass.IN),
-  ttl: FC_UINT32, // 32-bit Signed Integer Limits
-  data: fc.ipV4(),
-});
-
-const FC_CNAME_PTR_RECORD = fc.record({
-  name: fc.domain(),
-  type: fc.constantFrom(RType.CNAME, RType.PTR),
-  flush: fc.boolean(),
-  class: fc.constant(RClass.IN),
-  ttl: FC_UINT32,
-  data: fc.domain(),
-});
-
-const FC_TXT_RECORD = fc.record({
-  name: fc.domain(),
-  type: fc.constant(RType.TXT),
-  flush: fc.boolean(),
-  class: fc.constant(RClass.IN),
-  ttl: FC_UINT32,
-  data: fc.dictionary(
-    fc
-      .unicodeString({ minLength: 1 })
-      .filter((str) => str.indexOf('=') === -1 && str !== '__proto__'),
-    fc.unicodeString().filter((str) => str.indexOf('=') === -1),
-  ),
-});
-
-const FC_SRV_RECORD = fc.record({
-  name: fc.domain(),
-  type: fc.constant(RType.SRV),
-  flush: fc.boolean(),
-  class: fc.constant(RClass.IN),
-  ttl: FC_UINT32,
-  data: fc.record({
-    priority: FC_UINT16,
-    weight: FC_UINT16,
-    port: FC_UINT16,
-    target: fc.domain(),
-  }),
-});
-
-const FC_RESOURCE_RECORD = fc.oneof(
-  FC_AAAA_RECORD,
-  FC_A_RECORD,
-  FC_CNAME_PTR_RECORD,
-  FC_TXT_RECORD,
-  FC_SRV_RECORD,
-);
-
-const FC_PACKET_FLAGS = fc.record({
-  type: fc.constantFrom(PacketType.QUERY, PacketType.RESPONSE),
-  opcode: FC_PACKET_OPCODES,
-  rcode: FC_RCODES,
-  authoritativeAnswer: fc.boolean(),
-  truncation: fc.boolean(),
-  recursionDesired: fc.boolean(),
-  recursionAvailable: fc.boolean(),
-  zero: fc.boolean(),
-  authenticData: fc.boolean(),
-  checkingDisabled: fc.boolean(),
-});
+import { aaaaRecordArb, aRecordArb, cnamePtrRecordArb, packetArb, packetFlagsArb, packetOpCodeArb, questionRecordArb, srvRecordArb, txtRecordArb } from './utils';
 
 describe('/dns/utils.ts', () => {
   testProp('labels', [fc.domain()], (domain) => {
@@ -183,7 +77,7 @@ describe('/dns/utils.ts', () => {
     };
     expect(parser).toThrow(ErrorDNSParse);
   });
-  testProp('questions', [fc.array(FC_QUESTION)], (questions) => {
+  testProp('questions', [fc.array(questionRecordArb)], (questions) => {
     const generatedQuestions = generateQuestionRecords(questions);
     const parsedQuestions = parseQuestionRecords(
       generatedQuestions,
@@ -195,18 +89,7 @@ describe('/dns/utils.ts', () => {
   testProp(
     'packet flags',
     [
-      fc.record({
-        type: fc.constantFrom(PacketType.QUERY, PacketType.RESPONSE),
-        opcode: FC_PACKET_OPCODES,
-        rcode: FC_RCODES,
-        authoritativeAnswer: fc.boolean(),
-        truncation: fc.boolean(),
-        recursionDesired: fc.boolean(),
-        recursionAvailable: fc.boolean(),
-        zero: fc.boolean(),
-        authenticData: fc.boolean(),
-        checkingDisabled: fc.boolean(),
-      }),
+      packetFlagsArb,
     ],
     (flags) => {
       const encodedFlags = generatePacketFlags(flags);
@@ -214,7 +97,7 @@ describe('/dns/utils.ts', () => {
       expect(decodedFlags.data).toEqual(flags);
     },
   );
-  testProp('resource records a', [fc.array(FC_A_RECORD)], (resourceRecords) => {
+  testProp('resource records a', [fc.array(aRecordArb)], (resourceRecords) => {
     const generatedResourceRecords = generateResourceRecords(
       resourceRecords as any,
     );
@@ -227,22 +110,7 @@ describe('/dns/utils.ts', () => {
   });
   testProp(
     'resource records aaaa',
-    [fc.array(FC_AAAA_RECORD)],
-    (resourceRecords) => {
-      const generatedResourceRecords = generateResourceRecords(
-        resourceRecords as any,
-      );
-      const parsedResourceRecords = parseResourceRecords(
-        generatedResourceRecords,
-        generatedResourceRecords,
-        resourceRecords.length,
-      );
-      expect(parsedResourceRecords.data).toEqual(resourceRecords);
-    },
-  );
-  testProp(
-    'resource records aaaa',
-    [fc.array(FC_AAAA_RECORD)],
+    [fc.array(aaaaRecordArb)],
     (resourceRecords) => {
       const generatedResourceRecords = generateResourceRecords(
         resourceRecords as any,
@@ -257,7 +125,7 @@ describe('/dns/utils.ts', () => {
   );
   testProp(
     'resource records cname ptr',
-    [fc.array(FC_CNAME_PTR_RECORD)],
+    [fc.array(cnamePtrRecordArb)],
     (resourceRecords) => {
       const generatedResourceRecords = generateResourceRecords(
         resourceRecords as any,
@@ -272,7 +140,7 @@ describe('/dns/utils.ts', () => {
   );
   testProp(
     'resource records srv',
-    [fc.array(FC_SRV_RECORD)],
+    [fc.array(srvRecordArb)],
     (resourceRecords) => {
       const generatedResourceRecords = generateResourceRecords(
         resourceRecords as any,
@@ -287,7 +155,7 @@ describe('/dns/utils.ts', () => {
   );
   testProp(
     'resource records txt',
-    [fc.array(FC_TXT_RECORD)],
+    [fc.array(txtRecordArb)],
     (resourceRecords) => {
       const generatedResourceRecords = generateResourceRecords(
         resourceRecords as any,
@@ -300,23 +168,9 @@ describe('/dns/utils.ts', () => {
       expect(parsedResourceRecords.data).toEqual(resourceRecords);
     },
   );
-  testProp('packet flags', [FC_PACKET_FLAGS], (flags) => {
-    const generatedFlags = generatePacketFlags(flags);
-    const parsedFlags = parsePacketFlags(generatedFlags);
-    expect(parsedFlags.data).toEqual(flags);
-  });
   testProp(
     'packet',
-    [
-      fc.record({
-        id: FC_UINT16,
-        flags: FC_PACKET_FLAGS,
-        questions: fc.array(FC_QUESTION),
-        additionals: fc.array(FC_RESOURCE_RECORD),
-        answers: fc.array(FC_RESOURCE_RECORD),
-        authorities: fc.array(FC_RESOURCE_RECORD),
-      }),
-    ],
+    [packetArb],
     (packet) => {
       const generatedPacket = generatePacket(packet as any);
       const parsedPacket = parsePacket(generatedPacket);

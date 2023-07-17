62294776954e4b9d3387f59e0e04e09b92bd8585
Amy <50583248+amydevs@users.noreply.github.com>
2023-07-17 15:57:39 +1000

    WIP: options for socket are now set based on udp type

--- src/native/napi/socketUtils.cpp
+++ src/native/napi/socketUtils.cpp
@@ -3,8 +3,16 @@
 
 bool DisableMulticastAll(int sockfd) {
   int enable = 0;
-  int enableSize = sizeof(enable);
-  bool success = setsockopt(sockfd, IPPROTO_IP, IP_MULTICAST_ALL, &enable, enableSize) >= 0;
-  success = success && (setsockopt(sockfd, IPPROTO_IPV6, IPV6_MULTICAST_ALL, &enable, enableSize) >= 0);
+  int enablesize = sizeof(enable);
+  bool success = setsockopt(sockfd, IPPROTO_IP, IP_MULTICAST_ALL, &enable, enablesize) >= 0;
+
+  struct sockaddr_storage sockaddr;
+  socklen_t sockaddrsize = sizeof(sockaddr);
+  getsockname(sockfd, (struct sockaddr *) &sockaddr, &sockaddrsize);
+
+  if (sockaddr.ss_family == AF_INET6) {
+    success = success && (setsockopt(sockfd, IPPROTO_IPV6, IPV6_MULTICAST_ALL, &enable, enablesize) >= 0);
+  }
+
   return success;
 }

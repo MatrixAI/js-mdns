3ea2ce881b1e5234743c2aa86c912ca44c34bd7c
Brynley Llewellyn-Roux <blynblack@gmail.com>
2025-03-26 14:25:54 +1100

    fix: use `python3` naming not `python`

--- scripts/choco-install.ps1
+++ scripts/choco-install.ps1
@@ -30,7 +30,7 @@ if ( -not (Test-Path -Path "${PSScriptRoot}\..\tmp\chocolatey\$nodejs\$nodejs.20
 }
 
 # Install python v3.9.12 (will use cache if exists)
-$python = "python"
+$python = "python3"
 choco install "$python" --version='3.9.12' --require-checksums -y --no-progress
 # Internalise python to cache if doesn't exist
 if ( -not (Test-Path -Path "${PSScriptRoot}\..\tmp\chocolatey\$python\$python.3.9.12.nupkg" -PathType Leaf) ) {
